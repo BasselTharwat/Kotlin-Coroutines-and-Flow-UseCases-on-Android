fdc58dc30a82380b2d166081309de68656003bdd
BasselTharwat <bassel.tharwat01@gmail.com>
2025-01-29 12:46:16 +0200

    init commit

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/callbacks/SequentialNetworkRequestsCallbacksViewModel.kt
@@ -1,12 +1,65 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2.callbacks
 
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SequentialNetworkRequestsCallbacksViewModel(
     private val mockApi: CallbackMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
+    private var getAndroidVersionsCall: Call<List<AndroidVersion>>? = null
+    private var featuresOfMostRecentVersionCall: Call<VersionFeatures>? = null
+
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        getAndroidVersionsCall = mockApi.getRecentAndroidVersions()
+        getAndroidVersionsCall!!.enqueue(object : Callback<List<AndroidVersion>> {
+            override fun onResponse(
+                p0: Call<List<AndroidVersion>>,
+                p1: Response<List<AndroidVersion>>
+            ) {
+                if (p1.isSuccessful) {
+                    val mostRecentVersion = p1.body()!!.last()
+                    featuresOfMostRecentVersionCall =
+                        mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                    featuresOfMostRecentVersionCall!!.enqueue(object : Callback<VersionFeatures> {
+                        override fun onResponse(
+                            p0: Call<VersionFeatures>,
+                            p1: Response<VersionFeatures>
+                        ) {
+                            if (p1.isSuccessful) {
+                                uiState.value = UiState.Success(p1.body()!!)
+                            } else {
+                                uiState.value = UiState.Error("Network request failed")
+                            }
+                        }
+
+                        override fun onFailure(p0: Call<VersionFeatures>, p1: Throwable) {
+                            uiState.value = UiState.Error("Something unexpected happened!")
+                        }
+                    })
+                } else {
+                    uiState.value = UiState.Error("Network request failed")
+                }
+            }
+
+            override fun onFailure(p0: Call<List<AndroidVersion>>, p1: Throwable) {
+                uiState.value = UiState.Error("Something unexpected happened!")
+            }
+        })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        getAndroidVersionsCall?.cancel()
+        featuresOfMostRecentVersionCall?.cancel()
+
 
     }
 }
--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
@@ -1,12 +1,47 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2.rx
 
+import android.annotation.SuppressLint
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 class SequentialNetworkRequestsRxViewModel(
     private val mockApi: RxMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
+    private val disposables = CompositeDisposable()
+
+    @SuppressLint("CheckResult")
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        mockApi.getRecentAndroidVersions()
+            .flatMap { androidVersions ->
+                val recentVersion = androidVersions.last()
+                mockApi.getAndroidVersionFeatures(recentVersion.apiLevel)
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { versionFeatures ->
+                    uiState.value = UiState.Success(versionFeatures)
+                },
+                onError = {
+                    uiState.value = UiState.Error("Something went wrong !")
+                }
+            )
+            .addTo(disposables)
 
     }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        disposables.clear()
+    }
+
+
 }
