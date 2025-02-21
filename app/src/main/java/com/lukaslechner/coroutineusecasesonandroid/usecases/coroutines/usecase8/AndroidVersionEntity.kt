5098fdf00df7976bc318deced31dbaf330d2897b
BasselTharwat <bassel.tharwat01@gmail.com>
2025-02-21 19:43:11 +0200

    done with section 7 - room + coroutines

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/AndroidVersionEntity.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/AndroidVersionEntity.kt
@@ -7,8 +7,10 @@ import com.lukaslechner.coroutineusecasesonandroid.mock.AndroidVersion
 @Entity(tableName = "androidversions")
 data class AndroidVersionEntity(@PrimaryKey val apiLevel: Int, val name: String)
 
+// converts an entity to a list of Ui models
 fun List<AndroidVersionEntity>.mapToUiModelList() = map {
     AndroidVersion(it.apiLevel, it.name)
 }
 
+// converts a list of Ui models to an entity
 fun AndroidVersion.mapToEntity() = AndroidVersionEntity(this.apiLevel, this.name)
